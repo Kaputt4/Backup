771d17f6c2d58ec9e5f5d01f78fee8cd5b76e7e3
Javi Sánchez <73181608+Kaputt4@users.noreply.github.com>
2020-12-01 14:26:45 +0100

    Review english translation

--- backup.sh
+++ backup.sh
@@ -19,23 +19,23 @@ function checkCommands(){
 }
 #
 # Function copy()
-# Recieve an user like argument and checks if /home exits then compress it in /tmp
+# Receive an user like argument and checks if /home exits then compress it in /tmp
 #
 
 function copy(){
-	# Checks if user has got /home
+	# Checks if user has /home
 	if test `cat /etc/passwd | cut -d: -f1,6 | grep -w -c ^$USERV:/home` -gt 0
 	then
 		if test `cat /etc/passwd | cut -d: -f1,7 | grep -c ^$USERV:.*/nologin` -eq 0
 		then
-			echo -e " \nGoing to copy $USERV"
+			echo -e " \nCopying $USERV"
 			echo -e " \tCompressing: $(tree --noreport /home/$USERV | wc -l) files and directories"
 			tar --exclude=".*" -czf /tmp/$USERV"_"$(date +%Y_%m_%d).tar.gz -C /home/ $USERV 2> /dev/null #Compress /home
 			# Checks compressing errors
 			if test "${PIPESTATUS[0]}" -ne 2
 			then
 				((COUNT+=1))
-				echo -e "\n\tFile $USERV"_"$(date +%Y_%m_%d).tar.gz was succesfully created with permitions: `ls -l /tmp/$USERV"_"$(date +%Y_%m_%d).tar.gz | cut -d' ' -f1` "
+				echo -e "\n\tFile $USERV"_"$(date +%Y_%m_%d).tar.gz was succesfully created with permissions: `ls -l /tmp/$USERV"_"$(date +%Y_%m_%d).tar.gz | cut -d' ' -f1` "
 			else
 				# Send error to stderr
 				>&2 echo -e "\tError compressing files."
@@ -43,10 +43,10 @@ function copy(){
 			fi
 		else
 			# Send error to stderr
-			>&2 echo -e " \nUser $USERV can not be logged. /home does not exist."
+			>&2 echo -e " \nUser $USERV can not be used to login. /home does not exist."
 		fi
 	else
-		echo -e " \nUser $USERV do not have /home/$USERV"
+		echo -e " \nUser $USERV does not have /home/$USERV"
 	fi
 }
 
@@ -55,13 +55,13 @@ function copy(){
 echo -e "\n\n ____                _                  \n|  _ \              | |                 \n| |_) |  __ _   ___ | | __ _   _  _ __  \n|  _ <  / _\` | / __|| |/ /| | | || '_ \ \n| |_) || (_| || (__ |   < | |_| || |_) |\n|____/  \__,_| \___||_|\_\ \__,_|| .__/ \n                                 | |    \n                                 |_|    \n"
 echo "Created by Kaputt4 and Mikiped00"
 
-echo -e "\nUser $USER is going to do a security backup:"
+echo -e "\nUser $USER is going to do a backup:"
 echo -e "Date: `date`." 
 echo -e "Bash version: $BASH_VERSION "
 ERROR=0 # Saves error's value.
 COUNT=0 #  Counts .tar.gz files generated.
 
-# Check all necessary commands are installed
+# Checks that all necessary commands are installed
 checkCommands
 if test $ERROR -eq 0
 then
@@ -79,7 +79,7 @@ then
 		done
 		if  test $ERROR -ne 2 
 		then
-			# For each param, checks if it exists then call copy(). If it not exists then return 1 but keep executing. 
+			# For each param, checks if it exists, then calls copy(). If it not exists, returns error code 1 but keeps executing. 
 			for USERV in $@
 			do
 				EXIST=`cat /etc/passwd | cut -d: -f1,6 | grep -w -c $USERV` 
@@ -88,13 +88,13 @@ then
 					copy $USERV					
 				else
 					ERROR=1
-					# Envía error a stderr
-					>&2 echo -e "\nEl usuario $USERV no está dado de alta en el sistema."
+					# Sent error to stderr
+					>&2 echo -e "\nUser $USERV is not registered in the system."
 				fi
 			done
 		else
 			# Send error to stderr
-			>&2 echo -e "\nHas introducido un argumento con sintaxis incorrecta."
+			>&2 echo -e "\nArguments do not have the correct syntax."
 		fi
 	else
 		# Execute copy() for each user in /etc/passwd
@@ -104,7 +104,7 @@ then
 		done 
 	fi
 fi
-# If any compressed file were generated, then return 2.
+# If none files were generated, then return 2.
 if test $COUNT -eq 0; then ERROR=2; fi
 echo -e "\n$COUNT  compressed files were created."
 exit $ERROR
